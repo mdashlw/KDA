fe20d5cdaeb25ae0971c461f5f03f419718c2c27
mdashlw <mdashlw@gmail.com>
2019-05-23 11:04:47 +0300

    2.2.3
    * Fix PrivateSubCommand not taking top users from PrivateSubCommand

--- src/main/kotlin/ru/mdashlw/kda/command/PrivateSubCommand.kt
+++ src/main/kotlin/ru/mdashlw/kda/command/PrivateSubCommand.kt
@@ -3,7 +3,8 @@ package ru.mdashlw.kda.command
 import ru.mdashlw.kda.command.client.CommandClient
 
 abstract class PrivateSubCommand(parent: Command) : SubCommand(parent) {
-    open val users: Set<Long> = (parent as? PrivateCommand)?.users ?: emptySet()
+    open val users: Set<Long> =
+        ((parent as? PrivateCommand)?.users ?: (parent as? PrivateSubCommand)?.users).orEmpty()
 
     override fun checkAccess(event: Event): Boolean =
         (event.member.idLong.run { this == CommandClient.INSTANCE.owner || this in users })
