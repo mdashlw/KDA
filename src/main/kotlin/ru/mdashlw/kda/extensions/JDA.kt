e99451577881f53cfb98be9b874643e202aa56ed
mdashlw <mdashlw@gmail.com>
2019-05-27 16:26:57 +0300

    2.4.1
    * JDA#listenTo -> on

--- src/main/kotlin/ru/mdashlw/kda/extensions/JDA.kt
+++ src/main/kotlin/ru/mdashlw/kda/extensions/JDA.kt
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.hooks.EventListener
 
-inline fun <reified T : GenericEvent> JDA.listenTo(crossinline action: (event: T) -> Unit) {
+inline fun <reified T : GenericEvent> JDA.on(crossinline action: (event: T) -> Unit) {
     addEventListener(
         object : EventListener {
             override fun onEvent(event: GenericEvent) {
