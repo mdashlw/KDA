0a77e27d195d233f5838657e321efb8ce46cf9b5
mdashlw <mdashlw@gmail.com>
2019-06-01 15:45:12 +0300

    Fix compile error

--- src/main/kotlin/ru/mdashlw/kda/internal/command/commands/HelpCommand.kt
+++ src/main/kotlin/ru/mdashlw/kda/internal/command/commands/HelpCommand.kt
@@ -6,7 +6,7 @@ import ru.mdashlw.kda.internal.command.CommandClient
 
 object HelpCommand : Command() {
     override val name: String = "help"
-    override val aliases: List<String>? = listOf("commands", "cmds")
+    override val aliases: List<String> = listOf("commands", "cmds")
     override var usage: String = "[command]"
 
     override fun description(event: Event): String = event.localize("commands.help.description")
@@ -17,14 +17,14 @@ object HelpCommand : Command() {
             .distinct()
             .filter(Command::displayInHelp)
 
-        val self = jda.selfUser
+        val selfUser = jda.selfUser
 
         reply {
             description = localize("commands.help.reply.description", prefix)
 
             author {
-                name = localize("commands.help.reply.title", self.name)
-                icon = self.effectiveAvatarUrl
+                name = localize("commands.help.reply.title", selfUser.name)
+                icon = selfUser.effectiveAvatarUrl
             }
 
             field {
