083addd35dca029a52132eb5c67b325452366c4c
mdashlw <mdashlw@gmail.com>
2019-06-21 14:58:58 +0300

    4.0.0
    * Update artifactId to KDA
    * Move to GitHub

--- src/main/kotlin/ru/mdashlw/kda/internal/command/commands/HelpCommand.kt
+++ src/main/kotlin/ru/mdashlw/kda/internal/command/commands/HelpCommand.kt
@@ -5,8 +5,10 @@ import ru.mdashlw.kda.api.command.annotations.CommandFunction
 import ru.mdashlw.kda.internal.command.CommandClient
 
 object HelpCommand : Command() {
-    override val name: String = "help"
-    override val aliases: List<String> = listOf("commands", "cmds")
+    override val name: String
+        get() = "help"
+    override val aliases: List<String>
+        get() = listOf("commands", "cmds")
     override var usage: String = "[command]"
 
     override fun description(event: Event): String = event.localize("commands.help.description")
