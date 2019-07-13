0cb9d4f64e68c0c697574836cd167a017a23e19e
mdashlw <mdashlw@gmail.com>
2019-07-13 14:13:51 +0300

    5.2.1
    * Add nullableText(limit) and nullableWord()

--- src/main/kotlin/ru/mdashlw/kda/command/contexts/String.kt
+++ src/main/kotlin/ru/mdashlw/kda/command/contexts/String.kt
@@ -2,7 +2,11 @@ package ru.mdashlw.kda.command.contexts
 
 import ru.mdashlw.kda.command.Command
 
-fun Command.Context.word(fallback: String? = null): String = text(1, fallback)
+fun Command.Context.nullableWord(): String? = take()
+
+fun Command.Context.word(fallback: String? = null): String = take() ?: fallback ?: throw Command.Help()
+
+fun Command.Context.nullableText(limit: Int = args.size): String? = take(limit)
 
 fun Command.Context.text(limit: Int = args.size, fallback: String? = null): String =
     take(limit) ?: fallback ?: throw Command.Help()
